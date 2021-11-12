8a8934b920ff1aa2a267debbfba2bf00ade09b17
disenofq <fernando.quinteros@gmail.com>
2021-11-11 21:26:28 -0300

    Primer commit desafio arrays

---
+++ app.js
@@ -0,0 +1,15 @@
+let paises = ["Argentina", "Brasil", "Uruguay"];
+
+//Acceder mediante su índice
+let primero = paises[0];
+
+//Añadir un elemento
+
+let nuevaLongitud = paises.push("Chile");
+
+//Eliminar un elemento
+
+let ultimo = paises.pop(paises);
+console.log(paises);
+
+
